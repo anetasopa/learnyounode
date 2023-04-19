76a0de4ab481eeaa50c898a924ee4448315f11a4
Aneta <sopaaneta@gmail.com>
2023-04-19 15:50:40 +0200

    Added the code that was omitted

--- baby-steps.js
+++ baby-steps.js
@@ -6,7 +6,8 @@ for (let i = 2; i < process.argv.length; i++)
 console.log(result)
 
 // 2 solution
-
+// const result = process.argv.slice(2).reduce((acc, val)=> Number(acc) + Number(val), 0));
+// console.log(result);
 
 
 
