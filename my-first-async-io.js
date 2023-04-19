376a2bcb2d3698f2ebd652fee02123a8f94f4d2d
Aneta <sopaaneta@gmail.com>
2023-04-19 16:12:54 +0200

    Added my-first-async-io file

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
 
-let content = fs.readFile(process.argv[2]);
+let collback = fs.readFile(process.argv[2], function callback(err, data) {
+    let result = data.toString().split("\n").length - 1;
+    console.log(result);
+});
+
+
+
+
 
