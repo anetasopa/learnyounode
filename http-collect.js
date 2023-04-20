8ceff37e113a4424f3ea845d01ce04bcdc1a36ec
Aneta <sopaaneta@gmail.com>
2023-04-20 11:16:33 +0200

    Added http-client.js

---
+++ http-collect.js
@@ -0,0 +1,15 @@
+const bl = require('bl');
+const http = require('http');
+const url = process.argv[2];
+
+http.get(url, response => {
+    response.pipe(bl((err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+    }))
+})
+
